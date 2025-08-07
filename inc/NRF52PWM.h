ad2e0cac3e862ccdb54f8c3dcaa8873be2c03012
Kevin J Walters <kjw.github@gmail.com>
2025-08-07 16:05:25 +0100

    Tidying location of public member functions in class NRF52PWM. #59

--- inc/NRF52PWM.h
+++ inc/NRF52PWM.h
@@ -100,14 +100,6 @@ private:
     struct Nrf52PwmStats stats[NRF52PWM_STATS];           // statistics per output for debugging TODO use define size
 #endif
 
-    // TOOD Move this to bottom
-    /**
-     * Sets PWM hardware registers for chained buffers or simple one shot playback
-     * 
-     * @ param streamingMode If true, buffers will be streamed in order they are received. If false, the most recent buffer supplied always takes prescedence.
-     */
-    void setPwmLoopInten(bool streamingMode);
-
 public:  
     // The stream component that is serving our data
     DataSource      &upstream;
@@ -237,6 +229,12 @@ public:
      */
     int tryPull(uint8_t b);
 
+    /**
+     * Sets PWM hardware registers for chained buffers or simple one shot playback
+     * 
+     * @ param streamingMode If true, buffers will be streamed in order they are received. If false, the most recent buffer supplied always takes prescedence.
+     */
+    void setPwmLoopInten(bool streamingMode);
 };
 } // namespace codal
 
